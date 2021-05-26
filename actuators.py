c1d2b257a80b03cedd9f6e006ae40963e7d31fb6
Benaiah2320 <zsizemore2320@gmail.com>
2021-05-25 20:59:48 -0400

    Motor init values validation and tests

--- actuators.py
+++ actuators.py
@@ -2,26 +2,28 @@
 File for classes related to instantiating stepper motor objects
 """
 
-import queue
 
 class Step:
     """
     Step object, just enforces structure of step
     """
 
-    def __init__(self, direction, delay):
+    def __init__(self, direction: str, delay: int):
 
         # check direction value is 1 or -1
         if direction == 'ccw' or direction == 'cw':
             self.direction = direction
         else:
-            raise TypeError('Must be "ccw" or "cw"')
+            raise ValueError('Must be "ccw" or "cw"')
 
         # check delay is positive and over 5us
-        if delay >= 5:
-            self.delay = delay
+        if type(delay) == int:
+            if delay >= 5:
+                self.delay = delay
+            else:
+                raise ValueError('Must be greater than 5us')
         else:
-            raise TypeError('Must be greater than 5us')
+            raise TypeError('Must be an integer')
 
 
 class Motor:
@@ -35,13 +37,61 @@ class Motor:
             min_delay=5,
             max_delta_delay=5,
     ):
+        valid_steps_per_rev = [
+            400,
+            800,
+            1600,
+            3200,
+            6400,
+            12800,
+            25600,
+            1000,
+            2000,
+            4000,
+            5000,
+            8000,
+            10000,
+            20000,
+            25000,
+        ]
 
         # save init values
-        self.enable_pin = enable_pin
-        self.direction_pin = direction_pin
-        self.step_pin = step_pin
-        self.min_delay = min_delay
-        self.max_delta_delay = max_delta_delay
-        self.steps_per_rev = steps_per_rev
+        # pins must all be different, and must be strings or ints
+        pins = [enable_pin, direction_pin, step_pin]
+        if all((isinstance(pin, str) or isinstance(pin, int)) for pin in pins):
+            if len(pins) == len(set(pins)):
+                self.enable_pin = enable_pin
+                self.direction_pin = direction_pin
+                self.step_pin = step_pin
+            else:
+                raise ValueError('All pins must be different')
+        else:
+            raise TypeError
 
+        # delay must be int and greater than 5us
+        if type(min_delay) == int:
+            if min_delay >= 5:
+                self.min_delay = min_delay
+            else:
+                raise ValueError('Must be greater than 5us')
+        else:
+            raise TypeError('Must be an integer')
+
+        # delay must be int and greater than 5us
+        if type(max_delta_delay) == int:
+            if max_delta_delay >= 5:
+                self.max_delta_delay = max_delta_delay
+            else:
+                raise ValueError('Must be greater than 5us')
+        else:
+            raise TypeError('Must be an integer')
+
+        # steps per rev must be int and from list
+        if type(steps_per_rev) == int:
+            if steps_per_rev in valid_steps_per_rev:
+                self.steps_per_rev = steps_per_rev
+            else:
+                raise ValueError('Must be valid steps per rev from DM542T driver')
+        else:
+            raise TypeError('Must be an integer')
 
--- tests/test_actuators.py
+++ tests/test_actuators.py
@@ -2,7 +2,8 @@ import unittest
 
 from actuators import *
 
-class StepAdd(unittest.TestCase):
+
+class TestStep(unittest.TestCase):
 
     def test_init_dir_check_fail(self):
         """
@@ -11,7 +12,7 @@ class StepAdd(unittest.TestCase):
 
         values = [2, -5, 'str', '-1', 'cww']
         for i in values:
-            self.assertRaises(TypeError, Step, i, 5)
+            self.assertRaises(ValueError, Step, i, 5)
 
     def test_init_dir_check_pass(self):
         """
@@ -22,14 +23,23 @@ class StepAdd(unittest.TestCase):
         for i in values:
             self.assertEqual(i, Step(i, 5).direction)
 
-    def test_init_delay_fail(self):
+    def test_init_delay_int_fail(self):
         """
-        Test that delay fails when not 5 or greater
+        Test that delay fails when not int
         """
 
-        values = [4.9, 0, -5, -100, "5", "string"]
+        values = ["5", "string"]
         for i in values:
             self.assertRaises(TypeError, Step, 'cw', i)
+
+    def test_init_delay_min_fail(self):
+        """
+        Test that delay fails when not greater than 5us
+        """
+
+        values = [2, -5, -100, 4]
+        for i in values:
+            self.assertRaises(ValueError, Step, 'cw', i)
 
     def test_init_delay_pass(self):
         """
@@ -39,6 +49,155 @@ class StepAdd(unittest.TestCase):
         values = [100, 5]
         for i in values:
             self.assertEqual(i, Step('cw', i).delay)
+
+
+class TestMotor(unittest.TestCase):
+
+    def test_init_delay_int_fail(self):
+        """
+        Test that delay fails when not int
+        """
+
+        values = ["5", "string"]
+        for i in values:
+            self.assertRaises(TypeError, Motor, 1, 2, 3, 400, i)
+
+    def test_init_min_delay_min_fail(self):
+        """
+        Test that delay fails when not greater than 5us
+        """
+
+        values = [2, -5, -100, 4]
+        for i in values:
+            self.assertRaises(ValueError, Motor, 1, 2, 3, 400, i)
+
+    def test_init_min_delay_pass(self):
+        """
+        Tests that delay passes when 5 or greater
+        """
+
+        values = [100, 5]
+        for i in values:
+            self.assertEqual(i, Motor(1, 2, 3, 400, min_delay=i).min_delay)
+
+    def test_init_pin_str_int_fail(self):
+        """
+        Test that init fails when any pin value isn't an int or a string
+        """
+
+        values = {
+            'enable_pin': [1, Step('cw', 10)],
+            'direction_pin': [1, "GPIO 2"],
+            'step_pin': [[1, 1], "GPIO 2"],
+        }
+
+        for i in range(0, 2):
+            self.assertRaises(
+                TypeError,
+                Motor,
+                values['enable_pin'][i],
+                values['direction_pin'][i],
+                values['step_pin'][i],
+                100,
+            )
+
+    def test_init_pin_unequal_fail(self):
+        """
+        Tests that init fails when any pin values are the same
+        """
+
+        values = {
+            'enable_pin':    [1, "GPIO 2", 2, 4],
+            'direction_pin': [1, "GPIO 2", 3, 3],
+            'step_pin':      [1, 1, 3, 4],
+        }
+
+        for i in range(0, 4):
+            self.assertRaises(
+                ValueError,
+                Motor,
+                values['enable_pin'][i],
+                values['direction_pin'][i],
+                values['step_pin'][i],
+                400,
+            )
+
+    def test_init_pin_pass(self):
+        """
+        Tests that init passes when all pins are strings or ints and unique
+        """
+
+        values = {
+            'enable_pin': [1, "GPIO 2"],
+            'direction_pin': [2, "GPIO 3"],
+            'step_pin': [3, "GPIO 4"],
+        }
+
+        for i in range(0, 2):
+            test_motor = Motor(
+                values['enable_pin'][i],
+                values['direction_pin'][i],
+                values['step_pin'][i],
+                400,
+            )
+            self.assertEqual(values['enable_pin'][i], test_motor.enable_pin)
+            self.assertEqual(values['direction_pin'][i], test_motor.direction_pin)
+            self.assertEqual(values['step_pin'][i], test_motor.step_pin)
+
+    def test_init_max_delta_delay_int_fail(self):
+        """
+        Test that delay fails when not int
+        """
+
+        values = ["5", "string"]
+        for i in values:
+            self.assertRaises(TypeError, Motor, 1, 2, 3, 400, 10, i)
+
+    def test_init_max_delta_delay_min_fail(self):
+        """
+        Test that delay fails when not greater than 5us
+        """
+
+        values = [2, -5, -100, 4]
+        for i in values:
+            self.assertRaises(ValueError, Motor, 1, 2, 3, 400, 10, i)
+
+    def test_init_max_delta_delay_pass(self):
+        """
+        Tests that delay passes when 5 or greater
+        """
+
+        values = [100, 5]
+        for i in values:
+            self.assertEqual(i, Motor(1, 2, 3, 400, max_delta_delay=i).max_delta_delay)
+
+    def test_init_steps_int_fail(self):
+        """
+        Tests that init fails when steps_per_rev not int
+        """
+
+        values = ['100', [100, 200], 'string']
+        for i in values:
+            self.assertRaises(TypeError, Motor, 1, 2, 3, i)
+
+    def test_init_steps_valid_fail(self):
+        """
+        Tests that init fails when steps_per_rev not from list
+        of valid values on DM542T driver
+        """
+
+        values = [100, 900, 1000000]
+        for i in values:
+            self.assertRaises(ValueError, Motor, 1, 2, 3, i)
+
+    def test_init_steps_valid_pass(self):
+        """
+        Tests that init passes with a valid steps_per_rev value
+        """
+
+        values = [400, 6400, 8000]
+        for i in values:
+            self.assertEqual(i, Motor(1, 2, 3, i).steps_per_rev)
 
 
 if __name__ == '__main__':
